d35ca1b8236dd40aaf14b3633c540ea8d5e1cb49
rmnptrv <162408536+rmnptrv@users.noreply.github.com>
2024-12-19 15:55:55 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+print("Hello world!")
+
+#1st program
+print(9**0.5*5)
+
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3st program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(6 == 8)
+
+#4st program
+print("123.456")
+print(float("123.456") * 10)
+print(int(1234.56 % 10))
